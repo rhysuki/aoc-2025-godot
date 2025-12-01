3865e7af94fa039973a5c09adb22c049f5c7cd68
rhy <rhynomatty100@gmail.com>
2025-12-01 14:24:14 -0300

    Add Day 1 part 2

--- scripts/aoc/AocDay1.gd
+++ scripts/aoc/AocDay1.gd
@@ -15,6 +15,27 @@ func solve() -> int:
 	return result
 
 
+func solve_part_2() -> int:
+	var lines = get_lines(1)
+	var rotation: int = 50
+	var result: int = 0
+
+	for line in lines:
+		var rot := _get_rotation_change(line)
+		var sgn := signi(rot)
+		var remaining = abs(rot)
+
+		while remaining > 0:
+			remaining -= 1
+			rotation += sgn
+			rotation = wrapi(rotation, 0, 100)
+
+			if rotation == 0:
+				result += 1
+
+	return result
+
+
 func _get_rotation_change(line) -> int:
 	var dir = -1 if line.begins_with("L") else 1
 	var rot: int = int(line.substr(1))
